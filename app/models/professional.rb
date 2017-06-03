d9cc5d1c6d0fef19ead3c0f604bed1bcb4cf732a
railainesantos <railaine_santos@hotmail.com>
2017-06-03 20:22:15 -0300

    Alterando avatar

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,14 +1,14 @@
 class Professional < ActiveRecord::Base
   include ProfessionalAdmin
 
-  has_attached_file :photo,
+  has_attached_file :avatar,
   styles: {
     :medium => { :geometry => '500x500>'},
     :thumb  => { :geometry => '123x155>'},
     :small => { :geometry => '120x120>'}
   },
   :default_url => '/avatars/:style/missing.jpg'
-  validates_attachment_content_type :photo, :content_type => ['image/jpg', 'image/jpeg', 'image/png']
+  validates_attachment_content_type :avatar, :content_type => ['image/jpg', 'image/jpeg', 'image/png']
 
   validates :name, :role, :phone, :address, :city, presence: true, length: { maximum: 255 }
 
