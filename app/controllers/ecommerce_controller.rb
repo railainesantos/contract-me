b118e6c34d387c21e77406e3464fbcc359320631
railainesantos <railaine_santos@hotmail.com>
2017-06-03 22:11:08 -0300

    Add .

--- app/controllers/ecommerce_controller.rb
+++ app/controllers/ecommerce_controller.rb
@@ -1,5 +1,8 @@
 class EcommerceController < ApplicationController
 
   def index
+    @advertised_professionals = Professional.first(6)
+    @today_professionals = Professional.last(6)
+    @new_professionals = Professional.first(4)
   end
 end
