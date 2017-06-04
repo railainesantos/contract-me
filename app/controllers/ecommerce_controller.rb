041153d7f2bcb97db3e4cb91a8aa0b2022ed064c
railainesantos <railaine_santos@hotmail.com>
2017-06-04 01:21:19 -0300

    Ajustando scope

--- app/controllers/ecommerce_controller.rb
+++ app/controllers/ecommerce_controller.rb
@@ -2,7 +2,7 @@ class EcommerceController < ApplicationController
 
   def index
     @advertised_professionals = Professional.order("RANDOM()").limit(6)
-    @today_professionals = Professional.order("RANDOM()").limit(6)
+    @today_professionals = Professional.offset(rand(10)).last(6)
     @new_professionals = Professional.order("RANDOM()").limit(4)
   end
 end
